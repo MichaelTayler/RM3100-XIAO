829e34ece2158899c71014b673e5e0b8e7c116e4
Michael Tayler <79035992+MichaelTayler@users.noreply.github.com>
2021-04-08 01:06:28 +0200

    Update RM3100-XIAO.ino

--- Arduino/RM3100-XIAO.ino
+++ Arduino/RM3100-XIAO.ino
@@ -11,35 +11,40 @@ void setup() {
   pinMode(MOSI_pin, OUTPUT);
   pinMode(MISO_pin, INPUT); 
   pinMode(SCK_pin, OUTPUT);
-
   SPI.begin();  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
   Serial.begin(115200);  while (!Serial);
 }
 
 void loop() {
-  digitalWrite(SS_pin, LOW); 
+  digitalWrite(SS_pin, LOW); // Request measurement from sensor module
   SPI.transfer(0x00); 
-  SPI.transfer(B01110000);  // Mx+My+Mz read request (0xFF800000 = -8388608 to 0x7FFFFFFF = 8388607, approximately -800 uT to +800 uT).  Adjust for 1, 2 or 3 channels
+  SPI.transfer(B01110000);  // Mx+My+Mz read request (0xFF800000 = -8388608 to 0x7FFFFFFF = 8388607, approximately -800 uT to +800 uT).  
   digitalWrite(SS_pin, HIGH); 
   delay(10);
   
   digitalWrite(SS_pin, LOW); 
   SPI.transfer(0x0A4); 
-  rawX[0]=SPI.transfer(0x00); 
+  rawX[0]=SPI.transfer(0x00);  // Read in 24-bit data from sensor module, X channel, Most significant byte first
   rawX[1]=SPI.transfer(0x00); 
   rawX[2]=SPI.transfer(0x00); 
-  rawY[0]=SPI.transfer(0x00); 
+  rawY[0]=SPI.transfer(0x00);  // Read in 24-bit data from sensor module, Y channel
   rawY[1]=SPI.transfer(0x00); 
   rawY[2]=SPI.transfer(0x00); 
-  rawZ[0]=SPI.transfer(0x00); 
+  rawZ[0]=SPI.transfer(0x00);  // Read in 24-bit data from sensor module, Z channel
   rawZ[1]=SPI.transfer(0x00); 
   rawZ[2]=SPI.transfer(0x00); 
   digitalWrite(SS_pin, HIGH); 
   
-  mx=((65536*rawX[0]+256*rawX[1]+rawX[2])<<8)/800; Serial.print(mx); Serial.print(" ");
-  my=((65536*rawY[0]+256*rawY[1]+rawY[2])<<8)/800; Serial.print(my); Serial.print(" ");
-  mz=((65536*rawZ[0]+256*rawZ[1]+rawZ[2])<<8)/800; Serial.println(mz);   
-  delay(1); 
+  mx=(65536*rawX[0]+256*rawX[1]+rawX[2]);  // Reconstruct 24-bit sensor data values
+  my=(65536*rawY[0]+256*rawY[1]+rawY[2]); 
+  mz=(65536*rawZ[0]+256*rawZ[1]+rawZ[2]);  
+  
+  Serial.print(mx);    // print MX data to PC
+  Serial.print(" ");    
+  Serial.print(my);    // print MY data to PC
+  Serial.print(" "); 
+  Serial.println(mz);  // print MZ data to PC
+  
 }
 
 /*  digitalWrite(SS_pin, LOW); // Register write and read
